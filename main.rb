c0949663a905f2d618520f7257c989d4a1b0eded
emperorliu <mangomystery@gmail.com>
2014-10-07 15:21:37 -0700

    changes

--- main.rb
+++ main.rb
@@ -2,7 +2,9 @@ require 'rubygems'
 require 'sinatra'
 require 'pry'
 
-set :sessions, true
+use Rack::Session::Cookie, :key => 'rack.session',
+                           :path => '/',
+                           :secret => 'heythere' 
 
 BLACKJACK_AMOUNT = 21
 DEALER_MIN_HIT = 17
